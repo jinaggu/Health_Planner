55a58a9d2884e9bf1019d4107ae457d3e425bb27
DESKTOP-PGPLARB\shift <dlwls7108@naver.com>
2022-03-11 17:24:04 +0900

    음식 칼로리 검색기능 추가

--- src/main/resources/static/js/mealRecord.js
+++ src/main/resources/static/js/mealRecord.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
     $("#searchBtn").click(function() {
         let searchVal = $("#searchVal").val();
         $("#modalVal").text(searchVal);
+
+        if (searchVal == "" || searchVal == null) {
+            alert("검색어를 입력해주세요.");
+            return false;
+        }
+
         let data = {
             foodName : searchVal
         }
 
-        debugger;
         $.ajax({
             dataType: "json",
             type : "get",
@@ -16,8 +21,9 @@ $(document).ready(function() {
             success : function(res) {
                 console.log(res)
                 for (let i = 0; i < res.length; i++) {
-                    $("#tbody").append("<tr>" + "<td><input type='checkbox'></td>" +
+                    $("#tbody").append("<tr>" + "<td><input type='checkbox' name='check'></td>" +
                     "<td scope='row'>"+res[i].food_NM+"</td><td>"+res[i].nutr_CONT1+"</td>"+
+                    "<td style='display:none;'><input type='hidden' value='" + res[i].food_CD + "'></td>" +
                     "</tr>");
                 }
             },
@@ -27,6 +33,11 @@ $(document).ready(function() {
         });
 
     });
+
+    $("#calAdd").click(function() {
+
+    });
+
 
 });
 
